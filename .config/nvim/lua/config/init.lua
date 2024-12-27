606bc529494a8892ed8ce2b9978e8a80d8556f9a
alexandersep <sepelena@tcd.ie>
2024-12-27 23:11:23 +0000

    Add gruvbox

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -2,7 +2,8 @@ require('config.remap')
 require('config.set')
 
 -- ColourScheme for Neovim
---vim.o.background = "dark" -- or "light" for light mode
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
 
 -- Transparent background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
