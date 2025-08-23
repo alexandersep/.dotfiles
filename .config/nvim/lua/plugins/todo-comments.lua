18a645d8496195f9e417384a41c842661ecd3cb6
Alexander Sepelenco <sepelena@tcd.ie>
2025-08-23 14:42:27 +0100

    update todo

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -5,7 +5,20 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    colors = {
+        error = "#FFD6BA",
+        warning = "#DEE791",
+        info = "#BBDCE5",
+        hint = "#A3DC9A",
+        default = "#FFFFFF",
+    },
     keywords = {
+        FIX = {
+            icon = "F", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+        },
         TODO = { icon = "T", color = "info" },
         HACK = { icon = "H", color = "warning" },
         WARN = { icon = "W", color = "warning", alt = { "WARNING", "XXX" } },
@@ -14,6 +27,9 @@ return {
         TEST = { icon = "Te", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
     },
     highlight = {
+        keyword = "fg",
+        before = "",
+        after = "",
         pattern = [[.*<(KEYWORDS)]],
     },
     search = {
